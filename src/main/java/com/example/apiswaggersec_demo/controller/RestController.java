8594e95c5ff390dcbc9201ccaa968382423e4674
Marek Zvelebil <zvelebil.marek@gmail.com>
2024-03-03 20:48:30 +0100

    add RestController + 1 endpoint

---
+++ src/main/java/com/example/apiswaggersec_demo/controller/RestController.java
@@ -0,0 +1,23 @@
+package com.example.apiswaggersec_demo.controller;
+
+import com.example.apiswaggersec_demo.model.Movie;
+import com.example.apiswaggersec_demo.repository.MovieRepositor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+    final private MovieRepositor movieRepository;
+
+    public RestController(MovieRepositor movieRepository) {
+        this.movieRepository = movieRepository;
+    }
+    @GetMapping("/api/events")
+    public ResponseEntity<?> getEvents() {
+        List<Movie> movies = movieRepository.findAll();
+        return ResponseEntity.ok(movies);
+    }
+}
