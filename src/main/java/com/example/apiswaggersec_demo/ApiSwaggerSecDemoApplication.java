3ca9469672f345d4bb4cd63d02047f43c710db4a
Marek Zvelebil <zvelebil.marek@gmail.com>
2024-03-03 16:57:47 +0100

    initial commit

---
+++ src/main/java/com/example/apiswaggersec_demo/ApiSwaggerSecDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.apiswaggersec_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiSwaggerSecDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiSwaggerSecDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/apiswaggersec_demo/ApiSwaggerSecDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apiswaggersec_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSwaggerSecDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
