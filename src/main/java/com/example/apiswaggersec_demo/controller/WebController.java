3fe40402f53033b5339e8e827d7e189a1ab9cc0b
Marek Zvelebil <zvelebil.marek@gmail.com>
2024-03-04 12:34:37 +0100

    fixed web endpoint for search

--- src/main/java/com/example/apiswaggersec_demo/controller/WebController.java
+++ src/main/java/com/example/apiswaggersec_demo/controller/WebController.java
@@ -1,5 +1,6 @@
 package com.example.apiswaggersec_demo.controller;
 
+import com.example.apiswaggersec_demo.service.ApiService;
 import lombok.AllArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import lombok.extern.slf4j.Slf4j;
@@ -7,22 +8,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.Console;
+import java.net.URISyntaxException;
 
 @Controller
 @AllArgsConstructor
 @Slf4j
 public class WebController {
+    private final ApiService apiService;
     @GetMapping("/")
     public String index() {
         return "index";
     }
-    @GetMapping("/movie/find/{title}")
-    public String movie(@PathVariable String title, Model model) {
+    @PostMapping("/find")
+    public String movie(String title) {
         log.warn("title" + title);
+       // apiService.findMovie(title);
+
         //todo try find movie localy ,if not try find movie from api
-        return "index";
+        return "redirect:/";
     }
 
 }
--- src/main/java/com/example/apiswaggersec_demo/service/ApiService.java
+++ src/main/java/com/example/apiswaggersec_demo/service/ApiService.java
@@ -1,4 +1,39 @@
 package com.example.apiswaggersec_demo.service;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.*;
+
+import com.example.apiswaggersec_demo.apiKey.TheMovieDB;
+import com.example.apiswaggersec_demo.model.Movie;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+@Slf4j
 public class ApiService {
+
+    public void findMovie(String title){
+        final RestTemplate restTemplate = null;
+        String apiKey = new TheMovieDB().getApiKey();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setBearerAuth(apiKey);
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        //curl --request GET --url 'https://api.themoviedb.org/3/search/movie?query=Matrix&include_adult=false&language=en-US&page=1' \
+
+
+        try { URI uri = new URI("https://api.themoviedb.org/3/search/movie?query=" + title + "&include_adult=false&language=en-US&page=1");
+            RequestEntity<Void> requestEntity = new RequestEntity<>(headers, HttpMethod.GET, uri);
+            ResponseEntity<String> responseEntity = restTemplate.exchange(requestEntity, String.class);
+        } catch (Exception e) {
+            log.warn("error" + e);
+        }
+
+
+    }
 }
